b3b9340fecda21170a6957f3308df3cf0b90d1c9
Emil Kovacev <emil.kovacev@ibm.com>
2025-08-03 01:26:39 -0400

    Added more tests for integration and edge cases

--- tests/test_polynomials.rs
+++ tests/test_polynomials.rs
@@ -11,8 +11,21 @@ fn check_derivative(poly: &Polynomial, derivative: &Polynomial) {
     }
 }
 
+fn check_integral(poly: &Polynomial, integral: &Polynomial) {
+    assert_eq!(integral.terms.len(), poly.terms.len());
+    for i in 0..poly.terms.len() {
+        if poly.terms[i].coef == -1.0 {
+            assert_eq!(integral.terms[i].coef, poly.terms[i].coef.ln(), "Integral of 1/x is ln(x)");
+        } else {
+            assert_eq!(integral.terms[i].coef, poly.terms[i].coef / (poly.terms[i].exp + 1.0), "Coefficient was not multiplied correctly");
+        }
+        assert_eq!(integral.terms[i].exp, poly.terms[i].exp + 1.0, "Exponent was not incremented correctly");
+        assert_eq!(integral.terms[i].var, poly.terms[i].var, "Variable was mutated");
+    }
+}
+
 #[test]
-fn test_polynomials() {
+fn test_polynomial_differenciation() {
     // 3x^2 + 2x + 5
     let p1 = Polynomial::new(vec![
         Term::new(3.0, 'x', 2.0),
@@ -23,4 +36,42 @@ fn test_polynomials() {
     check_derivative(&p1, &p2);
     let p3 = p2.derivative();
     check_derivative(&p2, &p3);
+}
+
+#[test]
+fn test_polynomial_integration() {
+    // 3x^2 + 2x + 5
+    let p1 = Polynomial::new(vec![
+        Term::new(3.0, 'x', 2.0),
+        Term::new(2.0, 'x', 1.0),
+        Term::new(5.0, 'x', 0.0),
+    ]);
+    let p2 = p1.integral();
+    check_integral(&p1, &p2);
+    let p3 = p2.integral();
+    check_integral(&p2, &p3);
+}
+
+#[test]
+fn test_negative_inragration() {
+    // 3x^2 + 2x + 5
+    let p1 = Polynomial::new(vec![
+        Term::new(3.0, 'x', -2.0),
+        Term::new(2.0, 'x', 1.0),
+        Term::new(5.0, 'x', 0.0),
+    ]);
+    let p2 = p1.integral();
+    check_integral(&p1, &p2);
+    let p3 = p2.integral();
+    check_integral(&p2, &p3);
+}
+
+#[test]
+fn test_natural_log_integration() {
+    // 3x^2 + 2x + 5
+    let p1 = Polynomial::new(vec![
+        Term::new(3.0, 'x', -1.0),
+    ]);
+    let p2 = p1.integral();
+    check_integral(&p1, &p2);
 }
